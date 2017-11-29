2a2c6f331d71695dd1db53fd6f85ad87cbe8bdf0
HFrajacomo <henrique.frajacomo@gmail.com>
2017-11-29 19:10:54 -0300

    Radix Animation now functional

--- Teste.py
+++ Teste.py
@@ -11,10 +11,11 @@ WIDTH = 640
 HEIGHT = 480
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
 
-MAX_CHARS = 28
-N_Elements = 20
-Max_value = 100
+N_Elements = 320
+Max_value = 400
 
 def radixsort( aList ):
   RADIX = 3
@@ -40,31 +41,62 @@ def radixsort( aList ):
 	  for i in buck:
 		aList[a] = i
 		a += 1
+		screen.fill(BLACK)
+		DrawBars(aList)
+		DrawRedBar(a, aList[a-1])
+		pg.display.update()
+		time.sleep(0.005)
  
 	# move to next digit
 	placement *= RADIX
+
 
 	
 def DrawBars(Lista):
 	i = 0
 	for i in range (0, len(Lista)):
-		pg.draw.rect(screen, WHITE, (0+(i*32),HEIGHT-(Lista[i]*4.8),30,1000))
+		BlitTitle()
+		pg.draw.rect(screen, WHITE, ((i*(WIDTH/N_Elements)),HEIGHT-(Lista[i]*(HEIGHT/Max_value)),((WIDTH/N_Elements)-2),1000))
 		i = i + 1
 		
+def DrawRedBar(Index, ValIndex):
+	BlitTitle()
+	pg.draw.rect(screen, RED, ((Index*(WIDTH/N_Elements)),HEIGHT-(ValIndex*(HEIGHT/Max_value)),((WIDTH/N_Elements)-2),1000))
+	pg.display.update()
+	
+def DrawGreenBars(Lista):
+	i = 0
+	for i in range (0, len(Lista)):
+		pg.draw.rect(screen, GREEN, ((i*(WIDTH/N_Elements)),HEIGHT-(Lista[i]*(HEIGHT/Max_value)),((WIDTH/N_Elements)-2),1000))
+		BlitTitle()
+		pg.display.update()
+		time.sleep(0.001)
+		i = i + 1
+	
+def BlitTitle():
+	screen.blit(Surf,(10, 10, 250, 76))
+	
+#				  #
+#		Main	  #
+#				  #
 screen = pg.display.set_mode((WIDTH, HEIGHT))
+	
+Title = pg.font.Font("Arial.ttf", 24)
+Surf = Title.render("Radix Sort (LSD) - " + str(N_Elements) + " elements", 1, WHITE)
+BlitTitle()
+
 i=0
 Lista = []
-
-rd.seed(5)
+rd.seed(rd.randint(0,10000000000))
 
 for i in range(0,N_Elements):
-	Lista.append(rd.randrange(0, Max_value))
+	Lista.append(rd.randint(0, Max_value))
 
-DrawBars(Lista)
 	
-#print(Lista)
+
 radixsort(Lista)
-#print(Lista)
+DrawGreenBars(Lista)
+pg.display.update()
 
 while(True):
 	for event in pg.event.get():
